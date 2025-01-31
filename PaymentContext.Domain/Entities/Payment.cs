814929192a5f270db5abad4118f396bb8a28c0a4
Cleberton <clebertonfgc@gmail.com>
2025-01-31 19:14:29 -0300

    start models.

---
+++ PaymentContext.Domain/Entities/Payment.cs
@@ -0,0 +1,31 @@
+﻿namespace PaymentContext.Domain.Entities;
+
+public abstract class Payment
+{
+    public string Number { get; set; }
+    public DateTime PaidDate { get; set; }
+    public DateTime ExpiredDate { get; set; }
+    public decimal Total { get; set; }
+    public decimal TotalPaid { get; set; }
+    public string Payer { get; set; }
+    public string Document { get; set; }
+    public string Address { get; set; }
+}
+
+public class BoletoPayment : Payment
+{
+    public string BarCode { get; set; }
+    public string BoletoNumber { get; set; }
+}
+
+public class CreditCardPayment : Payment
+{
+    public int CardHolderName { get; set; }
+    public int CardNumber { get; set; }
+    public int LastTransactionNumber { get; set; }
+}
+
+public class PayPalPayment : Payment
+{
+    public int TransactionCode { get; set; }
+}
